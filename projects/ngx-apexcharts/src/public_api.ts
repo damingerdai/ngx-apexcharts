apexcharts/ng-apexcharts

refactor(public-api): removed wildcard exports

projects/ng-apexcharts/src/public_api.ts
@@ -2,6 +2,37 @@
  * Public API Surface of ng-apexcharts
  */
 
-export * from './lib/ng-apexcharts.module';
-export * from './lib/chart/chart.component';
-export * from './lib/model/apex-types';
+export { NgApexchartsModule } from './lib/ng-apexcharts.module';
+export { ChartComponent } from './lib/chart/chart.component';
+export {
+  ChartType,
+  ApexNoData,
+  ApexMarkers,
+  ApexTheme,
+  ApexGrid,
+  ApexForecastDataPoints,
+  ApexYAxis,
+  ApexXAxis,
+  ApexTooltip,
+  ApexResponsive,
+  ApexDataLabels,
+  ApexLegend,
+  ApexFill,
+  ApexPlotOptions,
+  ApexLocale,
+  TextAnnotations,
+  ImageAnnotations,
+  PointAnnotations,
+  YAxisAnnotations,
+  XAxisAnnotations,
+  AnnotationStyle,
+  AnnotationLabel,
+  ApexAnnotations,
+  ApexStroke,
+  ApexNonAxisChartSeries,
+  ApexAxisChartSeries,
+  ApexTitleSubtitle,
+  ApexStates,
+  ApexChart,
+  ApexOptions
+} from './lib/model/apex-types';

damingerdai/ngx-apexcharts

projects/ngx-apexcharts/src/public_api.ts
@@ -2,6 +2,37 @@
  * Public API Surface of ngx-apexcharts
  */
 
-export * from './lib/ngx-apexcharts.module';
-export * from './lib/chart/chart.component';
-export * from './lib/model/apex-types';
+export { NgApexchartsModule } from './lib/ngx-apexcharts.module';
+export { ChartComponent } from './lib/chart/chart.component';
+export {
+  ChartType,
+  ApexNoData,
+  ApexMarkers,
+  ApexTheme,
+  ApexGrid,
+  ApexForecastDataPoints,
+  ApexYAxis,
+  ApexXAxis,
+  ApexTooltip,
+  ApexResponsive,
+  ApexDataLabels,
+  ApexLegend,
+  ApexFill,
+  ApexPlotOptions,
+  ApexLocale,
+  TextAnnotations,
+  ImageAnnotations,
+  PointAnnotations,
+  YAxisAnnotations,
+  XAxisAnnotations,
+  AnnotationStyle,
+  AnnotationLabel,
+  ApexAnnotations,
+  ApexStroke,
+  ApexNonAxisChartSeries,
+  ApexAxisChartSeries,
+  ApexTitleSubtitle,
+  ApexStates,
+  ApexChart,
+  ApexOptions
+} from './lib/model/apex-types';