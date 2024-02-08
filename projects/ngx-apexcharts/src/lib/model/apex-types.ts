dca39edb8ba16a44923c7ffb4022a948746fd444
Lorenzo <lorenzo.piccinini@gmail.com>
2024-02-08 09:52:29 +0100

    Update apex-types.ts
    add stops param to ApexFill interface

--- projects/ngx-apexcharts/src/lib/model/apex-types.ts
+++ projects/ngx-apexcharts/src/lib/model/apex-types.ts
@@ -681,6 +681,7 @@ export interface ApexFill {
     inverseColors?: boolean;
     opacityFrom?: number | number[];
     opacityTo?: number | number[];
+    stops?:  number[];
     colorStops?: ApexColorStop[][] | ApexColorStop[];
   };
   image?: {
