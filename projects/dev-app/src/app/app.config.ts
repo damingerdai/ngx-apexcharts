a4b96e9f9a21942b63fbc7880682da38890f0ea1
Arthur Ming <guobin.ming@pwc.com>
2024-05-30 18:28:51 +0800

    chore(dev-app): open zoneless change detection

--- projects/dev-app/src/app/app.config.ts
+++ projects/dev-app/src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideClientHydration()]
 };
