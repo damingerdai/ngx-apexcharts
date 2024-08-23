3131380e34bddcb603d53eb86f2d6d0923fa28f1
Arthur Ming <guobin.ming@pwc.com>
2024-08-23 10:29:48 +0800

    feat(deps): update dependency apexcharts to v3.52.0

--- projects/ngx-apexcharts/schematics/ng-add/index.ts
+++ projects/ngx-apexcharts/schematics/ng-add/index.ts
@@ -1,18 +1,18 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree } from "@angular-devkit/schematics";
 import {
   NodePackageInstallTask,
   RunSchematicTask,
-} from '@angular-devkit/schematics/tasks';
-import { NgxApexchartNgAddSchema } from './schema';
-import { addPackageToPackageJson } from '../utils';
+} from "@angular-devkit/schematics/tasks";
+import { NgxApexchartNgAddSchema } from "./schema";
+import { addPackageToPackageJson } from "../utils";
 
 export default function (options: NgxApexchartNgAddSchema): Rule {
   return async (_host: Tree, _context: SchematicContext) => {
-    addPackageToPackageJson(_host, 'apexcharts', '~3.48.0');
+    addPackageToPackageJson(_host, "apexcharts", "3.52.0");
     const installTaskId = _context.addTask(new NodePackageInstallTask());
-    _context.addTask(new RunSchematicTask('ng-add-setup-project', options), [
+    _context.addTask(new RunSchematicTask("ng-add-setup-project", options), [
       installTaskId,
     ]);
-    _context.logger.log('info', '✅️ Added "ngx-apexcharts"');
+    _context.logger.log("info", '✅️ Added "ngx-apexcharts"');
   };
 }
