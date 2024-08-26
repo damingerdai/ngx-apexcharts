a8c559fc48bf5c7e6a86b51375f6ab42c1a24849
Arthur Ming <guobin.ming@pwc.com>
2024-08-26 17:47:04 +0800

    chore: lint-staged support angular porject dev-app

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   "src/*.ts?(x)": (filenames) =>
     filenames.length > 10
-      ? "ng lint ngx-apexcharts-demo"
-      : `eslint --format stylish ${filenames.join(" ")}`,
+      ? "ng lint ngx-apexcharts-demo --fix"
+      : `eslint --format stylish ${filenames.join(" ")} --fix`,
   "projects/ngx-apexcharts/*.ts?(x)": (filenames) =>
     filenames.length > 10
-      ? "ng lint ngx-apexcharts-demo"
-      : `eslint --format stylish ${filenames.join(" ")}`,
+      ? "ng lint ngx-apexcharts"
+      : `eslint --format stylish ${filenames.join(" ")} --fix`,
+  "projects/ngx-apexcharts/*.ts?(x)": (filenames) =>
+    filenames.length > 10
+      ? "ng lint dev-app"
+      : `eslint --format stylish ${filenames.join(" ")} --fix`,
 };
