a86dfebbd157ed18a2c7d5272b67a9981a771db7
Arthur Ming <guobin.ming@pwc.com>
2024-08-30 20:21:46 +0800

    feat(deps): update dependency apexcharts to v3.53.0

--- projects/ngx-apexcharts/schematics/ng-add/__tests__/index.spec.ts
+++ projects/ngx-apexcharts/schematics/ng-add/__tests__/index.spec.ts
@@ -1,33 +1,33 @@
-import 'jasmine';
+import "jasmine";
 import {
   SchematicTestRunner,
   UnitTestTree,
-} from '@angular-devkit/schematics/testing';
+} from "@angular-devkit/schematics/testing";
 import {
   Schema as ApplicationOptions,
   Style,
-} from '@schematics/angular/application/schema';
-import { Schema as WorkspaceOtions } from '@schematics/angular/workspace/schema';
-import * as path from 'path';
+} from "@schematics/angular/application/schema";
+import { Schema as WorkspaceOtions } from "@schematics/angular/workspace/schema";
+import * as path from "path";
 
 interface PackageJson {
   dependencies: Record<string, string>;
 }
 
-describe('ng add ngx-apexcharts', () => {
-  const collectionPath = path.join(__dirname, '../../collection.json');
-  const runner = new SchematicTestRunner('schematics', collectionPath);
+describe("ng add ngx-apexcharts", () => {
+  const collectionPath = path.join(__dirname, "../../collection.json");
+  const runner = new SchematicTestRunner("schematics", collectionPath);
 
   let appTree: UnitTestTree | undefined;
 
   beforeAll(async () => {
     const workspaceOptions: WorkspaceOtions = {
-      name: 'workspace',
-      newProjectRoot: 'projects',
-      version: '1.0.0',
+      name: "workspace",
+      newProjectRoot: "projects",
+      version: "1.0.0",
     };
     const appOptions: ApplicationOptions = {
-      name: 'ngx-apexcharts-app',
+      name: "ngx-apexcharts-app",
       inlineStyle: false,
       inlineTemplate: false,
       routing: false,
@@ -37,7 +37,7 @@ describe('ng add ngx-apexcharts', () => {
       standalone: false,
     };
     const appStandaloneOptions: ApplicationOptions = {
-      name: 'ngx-apexcharts-standalone-app',
+      name: "ngx-apexcharts-standalone-app",
       inlineStyle: false,
       inlineTemplate: false,
       routing: false,
@@ -47,50 +47,50 @@ describe('ng add ngx-apexcharts', () => {
       standalone: true,
     };
     appTree = await runner.runExternalSchematic(
-      '@schematics/angular',
-      'workspace',
+      "@schematics/angular",
+      "workspace",
       workspaceOptions,
     );
     appTree = await runner.runExternalSchematic(
-      '@schematics/angular',
-      'application',
+      "@schematics/angular",
+      "application",
       appOptions,
       appTree,
     );
     appTree = await runner.runExternalSchematic(
-      '@schematics/angular',
-      'application',
+      "@schematics/angular",
+      "application",
       appStandaloneOptions,
       appTree,
     );
   });
 
-  it('should add apexcharts to package.json', async () => {
-    const options = { project: 'ngx-apexcharts-app' };
-    const tree = await runner.runSchematic('ng-add', options, appTree);
-    expect(runner.tasks.some((task) => task.name === 'node-package'))
+  it("should add apexcharts to package.json", async () => {
+    const options = { project: "ngx-apexcharts-app" };
+    const tree = await runner.runSchematic("ng-add", options, appTree);
+    expect(runner.tasks.some((task) => task.name === "node-package"))
       .withContext(
-        'Expected the package manager to be scheduled in order to update lock files.',
+        "Expected the package manager to be scheduled in order to update lock files.",
       )
       .toBe(true);
-    expect(runner.tasks.some((task) => task.name === 'run-schematic'))
-      .withContext('Expected the setup-project schematic to be scheduled.')
+    expect(runner.tasks.some((task) => task.name === "run-schematic"))
+      .withContext("Expected the setup-project schematic to be scheduled.")
       .toBe(true);
     const packageJson = JSON.parse(
-      tree.readContent('/package.json'),
+      tree.readContent("/package.json"),
     ) as PackageJson;
-    expect(packageJson.dependencies['apexcharts']).toBe('3.52.0');
+    expect(packageJson.dependencies["apexcharts"]).toBe("3.53.0");
   });
 
-  it('should add ngx-apexcharts to AppModule', async () => {
-    const options = { project: 'ngx-apexcharts-app' };
+  it("should add ngx-apexcharts to AppModule", async () => {
+    const options = { project: "ngx-apexcharts-app" };
     const tree = await runner.runSchematic(
-      'ng-add-setup-project',
+      "ng-add-setup-project",
       options,
       appTree,
     );
     const appmodule = tree.readContent(
-      '/projects/ngx-apexcharts-app/src/app/app.module.ts',
+      "/projects/ngx-apexcharts-app/src/app/app.module.ts",
     );
     //expect(appmodule).toContain("import { NgxApexchartsModule } from 'ngx-apexcharts'");
     //expect(appmodule).toMatch(/imports:\s*\[\s*BrowserModule,\s*NgxApexchartsModule\s*\]/);
@@ -102,18 +102,18 @@ describe('ng add ngx-apexcharts', () => {
     );
   });
 
-  it('should add ngx-apexcharts to AppComponent', async () => {
-    const options = { project: 'ngx-apexcharts-standalone-app' };
+  it("should add ngx-apexcharts to AppComponent", async () => {
+    const options = { project: "ngx-apexcharts-standalone-app" };
     const tree = await runner.runSchematic(
-      'ng-add-setup-project',
+      "ng-add-setup-project",
       options,
       appTree,
     );
     const appconfig = tree.readContent(
-      'projects/ngx-apexcharts-standalone-app/src/app/app.config.ts',
+      "projects/ngx-apexcharts-standalone-app/src/app/app.config.ts",
     );
     expect(appconfig).toContain(
-      'import { NgxApexchartsModule } from \'ngx-apexcharts\'',
+      "import { NgxApexchartsModule } from 'ngx-apexcharts'",
     );
     expect(appconfig).toMatch(
       /providers:\s*\[.*importProvidersFrom\(NgxApexchartsModule\).*\]/,
