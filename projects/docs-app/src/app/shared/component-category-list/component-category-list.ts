3d800caa03bd87dd545573cc8db20db224abeaff
damingerdai <mingguobin@live.com>
2026-06-04 10:29:13 +0800

    chore: fix lint error

--- projects/docs-app/src/app/shared/component-category-list/component-category-list.ts
+++ projects/docs-app/src/app/shared/component-category-list/component-category-list.ts
@@ -6,28 +6,23 @@
  * found in the LICENSE file at https://angular.dev/license
  */
 
-import { Component, Input, OnDestroy, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
-import { MatRipple } from '@angular/material/core';
-import { combineLatest, Subscription } from 'rxjs';
+import { Component, Input } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { MatRipple } from "@angular/material/core";
 
-import { NavigationFocus } from '../../shared/navigation-focus/navigation-focus';
+import { NavigationFocus } from "../../shared/navigation-focus/navigation-focus";
 
-import { ComponentPageTitle } from '../page-title/page-title';
-import { DocItem } from '../documentation-items/documentation-items';
+import { DocItem } from "../documentation-items/documentation-items";
 
 @Component({
-    selector: 'app-component-category-list',
-    templateUrl: './component-category-list.html',
-    styleUrls: ['./component-category-list.scss'],
-    imports: [NavigationFocus, RouterLink, MatRipple],
+  selector: "app-component-category-list",
+  templateUrl: "./component-category-list.html",
+  styleUrls: ["./component-category-list.scss"],
+  imports: [NavigationFocus, RouterLink, MatRipple],
 })
 export class ComponentCategoryList {
-
-    @Input()
-    items: DocItem[] = [];
-    @Input()
-    categoryListSummary: string | undefined;
-
-
+  @Input()
+  items: DocItem[] = [];
+  @Input()
+  categoryListSummary: string | undefined;
 }
